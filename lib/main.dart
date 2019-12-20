ce867025ff985f29605421e9624e416855df429d
Olatunji <sirhollyfat@gmail.com>
2019-12-20 17:00:05 +0100

    Loop object and display

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,18 @@ class QuoteList extends StatefulWidget {
 
 class _QuoteListState extends State<QuoteList> {
 
-  List<String> quotes = [
-    'Be yourself; everyone else is already taken',
-    'I have nothing to declare except my genius',
-    'The truth is rarely pure and never simple'
+//  List<String> quotes = [
+//    'Be yourself; everyone else is already taken',
+//    'I have nothing to declare except my genius',
+//    'The truth is rarely pure and never simple'
+//  ];
+
+  List<Quote> quotes = [
+    Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken'),
+    Quote(author: 'Oscar Wilde', text: 'I have nothing to declare except my genius'),
+    Quote(author: 'Oscar Wilde', text: 'The truth is rarely pure and never simple')
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +34,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => Text(quote)
+        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')
         ).toList(),
       ),
     );
